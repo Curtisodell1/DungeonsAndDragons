2ba15a2c325a82f7effb99dd5d275e5f83b65ada
Curtis <curtisodell1@gmail.com>
2024-01-03 14:49:41 -0700

    framework for hover animation

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 function Landing() {
+    const handleMouseover = (e) => {
+        console.log(e.target.id)
+    }
+
     return(
         <div id="LandingPage">
-            <div id="TripticOne"></div>
-            <div id="TripticTwo"></div>
-            <div id="TripticThree"></div>
+            <div id="TripticContainer">
+                <div id="TripticOne"
+                onMouseOver={handleMouseover}
+                ></div>
+                <div id="TripticTwo"
+                onMouseOver={handleMouseover}
+                ></div>
+                <div id="TripticThree"
+                onMouseOver={handleMouseover}
+                ></div>
+            </div>
         </div>
     )
 }
