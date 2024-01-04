d6cc7818153f40924f292fc31f654b741c9ca26a
Curtis <curtisodell1@gmail.com>
2024-01-04 16:48:52 -0700

    working buttons in header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
+
     return(
         <div id="header">
-            <div></div>
+            <div id="ButtonContainer">
+                <Link to="/">
+                    <button>Home</button>
+                </Link>
+                <Link to="/booking">
+                    <button> Booking </button>
+                </Link>
+                <Link to="/reviews">
+                    <button> Reviews </button>
+                </Link>
+                <Link to="/about">
+                    <button> About </button>
+                </Link>
+            </div>
         </div>
     )
 }
