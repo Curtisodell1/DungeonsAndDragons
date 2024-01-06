7e18fa877d04ffca8d676225bb2148a24144ae36
Curtis <curtisodell1@gmail.com>
2024-01-05 22:36:19 -0700

    divs added to other pages

--- client/src/components/Booking.js
+++ client/src/components/Booking.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Booking() {
     return(
-        <>Booking</>
+        <div id="LandingPage">
+            <div id="TripticContainer">
+                <Link to="/">
+                    <div id="BookingPath"
+                    ></div>
+                </Link>
+                <Link to="/about">
+                    <div id="TripticTwo"
+                    ></div>
+                </Link>
+                <Link to="/reviews">
+                    <div id="TripticThree"
+                    ></div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- client/src/components/Reviews.js
+++ client/src/components/Reviews.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Reviews() {
     return(
-        <>Reviews</>
+        <div id="LandingPage">
+            <div id="TripticContainer">
+                <Link to="/booking">
+                    <div id="BookingPath"
+                    ></div>
+                </Link>
+                <Link to="/about">
+                    <div id="TripticTwo"
+                    ></div>
+                </Link>
+                <Link to="/">
+                    <div id="TripticThree"
+                    ></div>
+                </Link>
+            </div>
+        </div>
     )
 }
 
